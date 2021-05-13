668418f2d5f196e1a0a1e5059fc6b0bb1ceac973
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-13 23:52:42 +0200

    improved App.vue file by adding mechanism for showing or hiding completed tasks

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,18 @@
         <div class="col font-weight-bold">Task</div>
         <div class="col-2 font-weight-bold">Accomplished?</div>
       </div>
-      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
         <div class="col">{{t.action}}</div>
-        <div class="col-2">
+        <div class="col-2 text-center">
           <input type="checkbox" v-model="t.done" class="form-check-input" />
-          {{t.done}}
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">
+            Hide completed tasks
+          </label>
         </div>
       </div>
     </div>
@@ -32,8 +39,15 @@ export default {
                 {action: "Start looking for a new car", done: false},
                 {action: "Buy the biggest delicious Buenos Nachos Pizza", done: true},
                 {action: "Remove ex number from my telephone", done: false}],
+        hideCompleted: true
       }
+    },
+  //Model data to hide or show completed tasks
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
     }
+  }
 }
 </script>
 
